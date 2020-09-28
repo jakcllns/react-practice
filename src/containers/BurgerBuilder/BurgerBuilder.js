d5514f3c2503b6d26fd6148bb2f9208add281ea3
Jake Collins <jakcllns@gmail.com>
2020-09-28 09:16:45 -0400

    Fixed issue with ingredients put method

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -19,16 +19,22 @@ const BASE_PRICE = 4;
 
 class BurgerBuilder extends Component {
     state = {
-        ingredients: {
-            salad: 0,
-            bacon: 0,
-            cheese: 0,
-            meat: 0
-        },
+        ingredients: null,
         totalPrice: BASE_PRICE,
         purchaseable: false,
         purchasing: false,
-        loading: false
+        loading: false,
+        error: false
+    }
+
+    componentDidMount() {
+        instance.get('/ingredients')
+            .then(response => {
+                this.setState({ingredients: response.data});                
+            })
+            .catch(error => {
+                this.setState({error: true})
+            });
     }
 
     updatePurchaseState(ingredients) {
@@ -87,7 +93,7 @@ class BurgerBuilder extends Component {
     purchaseContinueHandler = () => {
         // alert('Purchase complate!');
         this.setState({ loading: true});
-        instance.post('/order', {totalPrice: this.state.totalPrice, ingredients: this.state.ingredients})
+        instance.post('/orders', {totalPrice: this.state.totalPrice, ingredients: this.state.ingredients})
             .then(response => this.setState({loading: false, purchasing: false}))
             .catch(error => this.setState({loading: false, purchasing: false}));
         
@@ -102,32 +108,43 @@ class BurgerBuilder extends Component {
             disabledInfo[key] = disabledInfo[key] <= 0;
         }
 
-        let orderSummary = <OrderSummary 
-        purchaseCancelled={this.purchaseCancelHandler}
-        purchaseContinue={this.purchaseContinueHandler}
-        totalPrice={this.state.totalPrice}
-        ingredients={this.state.ingredients}/>
+        let orderSummary = null;
 
         if (this.state.loading){
             orderSummary = <Spinner />
         }
 
+        let burger = this.state.error ? <p>Ingredients cannot be loaded!</p> :<Spinner />
 
+        if (this.state.ingredients){
+            burger = (
+                <Auxillary>
+                    <Burger ingredients={this.state.ingredients}/>
+                    <BuildControls 
+                        ingredientAdded={this.addIngredientHandler}
+                        ingredientRemoved={this.removeIngredientHandler}
+                        disabled={disabledInfo}
+                        totalPrice={this.state.totalPrice}
+                        purchaseable={this.state.purchaseable}
+                        orderNowClick={this.purchaseHandler}
+                    />
+                </Auxillary>
+            );
+
+            orderSummary = <OrderSummary 
+                purchaseCancelled={this.purchaseCancelHandler}
+                purchaseContinue={this.purchaseContinueHandler}
+                totalPrice={this.state.totalPrice}
+                ingredients={this.state.ingredients}/>;
+        } 
+        
         return (
             
             <Auxillary>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
                     {orderSummary}
                 </Modal>
-                <Burger ingredients={this.state.ingredients}/>
-                <BuildControls 
-                    ingredientAdded={this.addIngredientHandler}
-                    ingredientRemoved={this.removeIngredientHandler}
-                    disabled={disabledInfo}
-                    totalPrice={this.state.totalPrice}
-                    purchaseable={this.state.purchaseable}
-                    orderNowClick={this.purchaseHandler}
-                />
+                {burger}
             </Auxillary>            
         );
     }
