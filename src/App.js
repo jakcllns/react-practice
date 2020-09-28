be40ee789cdf2b0d9fc7cf30a44fc5429a1908e5
Jake Collins <jakcllns@gmail.com>
2020-09-28 09:34:05 -0400

    Remove unneccessary code from App.js

--- src/App.js
+++ src/App.js
@@ -7,12 +7,6 @@ class App extends Component {
     show: true
   }
 
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({show: false});
-    }, 5000);
-  }
-  
   render() {
     return(
       <div>
