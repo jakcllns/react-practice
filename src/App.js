4a03fa560407b1f874909f819735913a3b2aaf6c
Jake Collins <jakcllns@gmail.com>
2020-09-29 06:43:24 -0400

    Created checkout container with checkout summary Added react router to begin adding routing to the app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from "react";
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+import Checkout from "./containers/Checkout/Checkout";
 
 class App extends Component {
   state = {
-    show: true
+    
   }
 
   render() {
     return(
       <div>
         <Layout>
-          {this.state.show ? <BurgerBuilder/> : null}
+          <BurgerBuilder/>
+          <Checkout />
         </Layout>
         
       </div>
